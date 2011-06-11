06c444036950dc362b3b448983811b425bf449ff
D Sofer <d@sofer.com>
2011-06-11 19:46:00 +0100

    blog

--- post.rb
+++ post.rb
@@ -5,6 +5,7 @@ path = '_posts/'
 if ARGV[3] and ARGV[0] =~ /\d\d\d\d-\d\d-\d\d/
   date, title, cat, author, top = ARGV 
   top ||= false
+  kind = (['social','energy','growing'].include? cat) ? cat : 'home'
   file = path + date+'-'+title.gsub(' ', '-').downcase.gsub(/[^a-z-]/, '') + '.textile'
   outline = <<OUTLINE
 ---
@@ -12,7 +13,7 @@ layout: post
 top: #{top}
 title: #{title}
 author: #{author}
-kind: home
+kind: #{kind}
 categories: #{cat}
 tags: [] 
 summary: "" 
@@ -31,16 +32,6 @@ arg1: date in YYYY-MM-DD format
 arg2: title
 arg3: category
 arg4: author
-arg5: top T or F
+arg5: leave blank if not a top story
 USAGE
 end
-
-__END__
-
-As part of their Spring Film Season, Transition Kentish Town are showing _The Farmer and the Horse_ at Annie's Bar. 
-
-It's a fascinating immersion into the world of three young people trying to get into agriculture who have fallen in love with working with horses. A vision of how farming might be in a lower-energy world.
-
-All film showings are in the downstairs room at Annie's Bar, 180 Kentish Town Road, NW5 2AE.
-
-These are relaxed occasions and they usually have a friendly discussion afterwards. A &pound;2 donation is requested.
